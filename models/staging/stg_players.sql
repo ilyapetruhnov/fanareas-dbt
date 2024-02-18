71728d407a5886857f75bb42f5ab3c1c4b5e8127
ilyapetruhnov <petrukha16@gmail.com>
2024-02-18 20:30:00 +0100

    wip quizzes

--- models/staging/stg_players.sql
+++ models/staging/stg_players.sql
@@ -75,34 +75,35 @@ squads_seasons_teams as (
 
 final as (
 
-select
-    players.id as player_id,
-    squad_id,
-    countries_continents.country_id as nationality_id,
-    countries_continents.continent as continent,
-    countries_continents.country as nationality,
-    cities.name as city,
-    types.id as position_id,
-    types.name as position,
-    squads_seasons_teams.team_id as team_id,
-    squads_seasons_teams.team as team,
-    squads_seasons_teams.season_id as season_id,
-    squads_seasons_teams.season,
-    players.name as fullname,
-    firstname,
-    lastname,
-    height,
-    weight,
-    date_of_birth
-    from players
-    left join countries_continents
-    on players.nationality_id = countries_continents.country_id
-    left join cities
-    on players.city_id = cities.id
-    left join types
-    on detailed_position_id = types.id
-    left join squads_seasons_teams
-    on squads_seasons_teams.player_id = players.id
+    select
+        players.id as id,
+        players.id as player_id,
+        squad_id,
+        countries_continents.country_id as nationality_id,
+        countries_continents.continent as continent,
+        countries_continents.country as nationality,
+        cities.name as city,
+        types.id as position_id,
+        types.name as position,
+        squads_seasons_teams.team_id as team_id,
+        squads_seasons_teams.team as team,
+        squads_seasons_teams.season_id as season_id,
+        squads_seasons_teams.season,
+        players.name as fullname,
+        firstname,
+        lastname,
+        height,
+        weight,
+        date_of_birth
+        from players
+        left join countries_continents
+        on players.nationality_id = countries_continents.country_id
+        left join cities
+        on players.city_id = cities.id
+        left join types
+        on detailed_position_id = types.id
+        left join squads_seasons_teams
+        on squads_seasons_teams.player_id = players.id
 )
 
 select * from final
