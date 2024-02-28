5fb9d8eff2451bfc8a0e8847bd1faee23681725a
ilyapetruhnov <petrukha16@gmail.com>
2024-02-28 16:15:13 +0100

    adding jobs

--- dagster_fanareas/dagster_fanareas/quizzes/templates.py
+++ dagster_fanareas/dagster_fanareas/quizzes/templates.py
@@ -3,7 +3,35 @@ import pandas as pd
 import random
 from dagster_fanareas.ops.utils import post_json, create_db_session
 
-def guess_player_template(quiz_type:str, query:str, statement: str, *cols):
+def quiz_template(quiz_type: int, title:str, description: str, q_lst: list):
+    # engine = create_db_session()
+    # df = pd.read_sql(query, con=engine)
+    
+    # q_lst = []
+    # for i in range(10):
+    #     dimension = cols[0]
+    #     val_dim = df[dimension].unique()[i]
+    #     sample_df = df[df[dimension]==val_dim].sample(n=4)
+    #     correct_idx = random.randint(0, 3)
+    #     correct_row = sample_df.iloc[correct_idx]
+    #     correct_vals = [correct_row[i] for i in cols]
+    #     question = statement.format(*correct_vals)
+    #     options = list(sample_df['fullname'])
+    #     correct_response = correct_row['fullname']
+    #     question = {
+    #     "description": question,
+    #     "quizQuestionOptions": options,
+    #     "correctAnswer": correct_response
+    #                 }
+        # q_lst.append(question)
+    json_data = {"title": title,
+                     "type": quiz_type,
+                    "description": description,
+                    'questions': q_lst}
+        
+    return json_data
+
+def generate_quiz_questions(query: str, statement: str, *cols) -> list:
     engine = create_db_session()
     df = pd.read_sql(query, con=engine)
     
@@ -24,16 +52,14 @@ def guess_player_template(quiz_type:str, query:str, statement: str, *cols):
         "correctAnswer": correct_response
                     }
         q_lst.append(question)
-        json_data = {"title": "Guess the player",
-                     "type": quiz_type,
-                    "description": "Answer 10 questions",
-                    'questions': q_lst}
-        
-    return json_data
+    return q_lst
+
+
 
 
 @asset( group_name="templates", compute_kind="pandas")
-def quiz_player_shirt_number(context) -> dict:
+def quiz_player_shirt_number(context) -> list:
+
     query="""
         SELECT
         firstname,
@@ -50,20 +76,19 @@ def quiz_player_shirt_number(context) -> dict:
         and array_length(t.team,1) = 1
         and is_active = true
         """
-    statement = "Which player currently plays for {} under {} shirt number?"
-    quiz_type=0
-    json_data = guess_player_template(quiz_type, query, statement, 'team', 'jersey_number')
-    return json_data
+    statement = "Which player currently plays for {} under {} jersey number?"
+    q_list = generate_quiz_questions(query, statement, 'team', 'jersey_number')
+    return q_list
 
 @asset( group_name="templates", compute_kind="pandas")
-def quiz_player_age_nationality(context) -> dict:
+def quiz_player_age_nationality(context) -> list:
     query="""
         SELECT
         firstname,
         lastname,
         fullname,
         nationality,
-        date_part('year', cast(date_of_birth as date) ) as birth_year,
+        int(date_part('year', cast(date_of_birth as date)) ) as birth_year,
         array_to_string(team, '/') as team,
         array_to_string(jersey_number, '/') as jersey_number,
         t.season
@@ -77,12 +102,12 @@ def quiz_player_age_nationality(context) -> dict:
         and is_active = true
         """
     statement = "Which player was born in {} in {}?"
-    quiz_type=0
-    json_data = guess_player_template(quiz_type, query, statement, 'birth_year', 'nationality')
-    return json_data
+    q_list = generate_quiz_questions(query, statement, 'nationality', 'birth_year')
+
+    return q_list
 
 @asset( group_name="templates", compute_kind="pandas")
-def quiz_player_age_team(context) -> dict:
+def quiz_player_age_team(context) -> list:
     query="""
         SELECT
         firstname,
@@ -103,13 +128,14 @@ def quiz_player_age_team(context) -> dict:
         and is_active = true
         """
     statement = "Which player currently plays for team {} and was born in {}?"
-    quiz_type=0
-    json_data = guess_player_template(quiz_type, query, statement, 'team', 'birth_year')
-    return json_data
+    q_list = generate_quiz_questions(query, statement, 'team', 'birth_year')
+
+    return q_list
 
 
 @asset( group_name="templates", compute_kind="pandas")
-def quiz_player_2_clubs_played(context) -> dict:
+def quiz_player_2_clubs_played(context) -> list:
+
     query="""
             WITH vw as (
             SELECT
@@ -135,12 +161,11 @@ def quiz_player_2_clubs_played(context) -> dict:
             where team != transfer_from_team
     """
     statement = "Which player played for {} and {} in his career?"
-    quiz_type=1
-    json_data = guess_player_template(quiz_type, query, statement, 'team', 'transfer_from_team')
-    return json_data
+    q_list = generate_quiz_questions(query, statement, 'team', 'transfer_from_team')
+    return q_list
 
 @asset( group_name="templates", compute_kind="pandas")
-def quiz_player_transferred_from_to(context) -> dict:
+def quiz_player_transferred_from_to(context) -> list:
     query="""
             WITH vw as (
             SELECT
@@ -166,27 +191,64 @@ def quiz_player_transferred_from_to(context) -> dict:
             where team != transfer_from_team
     """
     statement = "Which player had a transfer from {} to {} in the {} season?"
+    q_list = generate_quiz_questions(query, statement, 'transfer_from_team', 'team', 'season')
+    return q_list
+
+
+# @asset(group_name="templates")
+# def post_quiz_player_shirt_number(quiz_player_shirt_number: dict) -> bool:
+#     return post_json(quiz_player_shirt_number)
+
+# @asset(group_name="templates")
+# def post_quiz_player_age_nationality(quiz_player_age_nationality: dict) -> bool:
+#     return post_json(quiz_player_age_nationality)
+
+# @asset(group_name="templates")
+# def post_quiz_player_age_team(quiz_player_age_team: dict) -> bool:
+#     return post_json(quiz_player_age_team)
+
+# @asset(group_name="templates")
+# def post_quiz_player_2_clubs_played(quiz_player_2_clubs_played: dict) -> bool:
+#     return post_json(quiz_player_2_clubs_played)
+
+# @asset(group_name="templates")
+# def post_quiz_player_transferred_from_to(quiz_player_transferred_from_to: dict) -> bool:
+#     return post_json(quiz_player_transferred_from_to)
+
+
+# @asset(group_name="templates")
+# def post_transfer_quiz(quiz_player_transferred_from_to: dict, quiz_player_2_clubs_played: dict) -> bool:
+
+#     post_json(quiz_player_transferred_from_to)
+#     post_json(quiz_player_2_clubs_played)
+#     return post_json(quiz_player_transferred_from_to)
+
+@asset(group_name="templates")
+def post_guess_the_player_quiz(quiz_player_age_nationality: dict, quiz_player_age_team: dict, quiz_player_shirt_number: dict) -> bool:
+    title = "Guess the player"
+    description = "Guess 10 football players from the Premier League"
+    l1 = quiz_player_age_nationality()
+    l2 = quiz_player_age_team()
+    l3 = quiz_player_shirt_number()
+    combined_q_list = l1 + l2 + l3
+    random.shuffle(combined_q_list)
+    result_list = combined_q_list[:9]
+    quiz_type=0
+    json_data = quiz_template(quiz_type, title, description, result_list)
+
+    return post_json(json_data)
+
+
+@asset(group_name="templates")
+def post_guess_the_player_quiz(quiz_player_transferred_from_to: dict, quiz_player_2_clubs_played: dict) -> bool:
+    title = "Daily transfers"
+    description = "Answer 10 question about Premier League transfers"
+    l1 = quiz_player_transferred_from_to()
+    l2 = quiz_player_2_clubs_played()
+    combined_q_list = l1 + l2
+    random.shuffle(combined_q_list)
+    result_list = combined_q_list[:9]
     quiz_type=1
-    json_data = guess_player_template(quiz_type, query, statement, 'transfer_from_team', 'team', 'season')
-    return json_data
+    json_data = quiz_template(quiz_type, title, description, result_list)
 
-
-@asset(group_name="templates")
-def post_quiz_player_shirt_number(quiz_player_shirt_number: dict) -> bool:
-    return post_json(quiz_player_shirt_number)
-
-@asset(group_name="templates")
-def post_quiz_player_age_nationality(quiz_player_age_nationality: dict) -> bool:
-    return post_json(quiz_player_age_nationality)
-
-@asset(group_name="templates")
-def post_quiz_player_age_team(quiz_player_age_team: dict) -> bool:
-    return post_json(quiz_player_age_team)
-
-@asset(group_name="templates")
-def post_quiz_player_2_clubs_played(quiz_player_2_clubs_played: dict) -> bool:
-    return post_json(quiz_player_2_clubs_played)
-
-@asset(group_name="templates")
-def post_quiz_player_transferred_from_to(quiz_player_transferred_from_to: dict) -> bool:
-    return post_json(quiz_player_transferred_from_to)
+    return post_json(json_data)
