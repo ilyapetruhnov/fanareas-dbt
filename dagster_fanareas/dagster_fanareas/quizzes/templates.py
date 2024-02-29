b956353927f87205f324a35aeea9b90b8775187a
ilyapetruhnov <petrukha16@gmail.com>
2024-02-29 17:29:57 +0100

    adding jobs

--- dagster_fanareas/dagster_fanareas/quizzes/templates.py
+++ dagster_fanareas/dagster_fanareas/quizzes/templates.py
@@ -11,6 +11,50 @@ def quiz_template(quiz_type: int, title:str, description: str, result_list: list
         
     return json_data
 
+
+# def generate_quiz_questions(query: str, statement: str, *cols) -> list:
+#     engine = create_db_session()
+#     df = pd.read_sql(query, con=engine)
+#     if cols:
+#         q_lst = []
+#         for i in range(10):
+#             dimension = cols[0]
+#             val_dim = df[df[dimension].map(df[dimension].value_counts()) > 4][dimension].value_counts().index.unique()[i]
+#             # val_dim = df[dimension].unique()[i]
+#             sample_df = df[df[dimension]==val_dim].sample(n=4)
+#             correct_idx = random.randint(0, 3)
+#             correct_row = sample_df.iloc[correct_idx]
+#             correct_vals = [correct_row[i] for i in cols]
+#             question = statement.format(*correct_vals)
+#             options = list(sample_df['fullname'])
+#             correct_response = correct_row['fullname']
+#             question = {
+#             "description": question,
+#             "quizQuestionOptions": options,
+#             "correctAnswer": correct_response
+#                         }
+#             q_lst.append(question)
+#     else:
+#         q_lst = []
+#         for i in range(10):
+
+#             sample_df = df.sample(n=4).sort_values('height')
+#             # height = sample_df.iloc[3]['height']
+#             # player_name = sample_df.iloc[3]['player_name']
+#             # correct_vals = [correct_row[i] for i in cols]
+#             question = statement.format(*correct_vals)
+#             options = list(sample_df['fullname'])
+#             correct_response = sample_df.iloc[3]['player_name']
+#             question = {
+#             "description": question,
+#             "quizQuestionOptions": options,
+#             "correctAnswer": correct_response
+#                         }
+#             q_lst.append(question)
+
+#     return q_lst
+
+
 def generate_quiz_questions(query: str, statement: str, *cols) -> list:
     engine = create_db_session()
     df = pd.read_sql(query, con=engine)
@@ -136,7 +180,7 @@ def quiz_player_age_team(context) -> list:
             next_team != team
             or next_birth_year != birth_year
             """
-    statement = "Which player currently plays for team {} and was born in {}?"
+    statement = "Which player currently plays for {} and was born in {}?"
     q_list = generate_quiz_questions(query, statement, 'team', 'birth_year')
 
     return q_list
@@ -241,12 +285,50 @@ def quiz_player_transferred_from_to(context) -> list:
     q_list = generate_quiz_questions(query, statement, 'transfer_from_team', 'team', 'season')
     return q_list
 
+@asset( group_name="templates", compute_kind="pandas")
+def quiz_player_height(context) -> list:
+    statement = "Guess the tallest player from the following players"
+    i = random.randint(0, 40)
+    query=f"""
+            with vw as (
+                    SELECT
+                    height,
+                    array_agg(fullname) as fullname
+                    FROM
+                    dim_players
+                    where height is not null
+                    and height != 0
+                    and is_active = true
+            group by height
+            )
+            select
+            height,
+            fullname[{i}] AS fullname
+            from vw
+        """
+    engine = create_db_session()
+    df = pd.read_sql(query, con=engine)
+    q_lst = []
+    for i in range(10):
+
+        sample_df = df.sample(n=4).sort_values('height')
+        options = list(sample_df['fullname'])
+        correct_response = sample_df.iloc[3]['fullname']
+        question = {
+        "description": statement,
+        "quizQuestionOptions": options,
+        "correctAnswer": correct_response
+                    }
+        q_lst.append(question)
+    
+    return q_lst
+
 
 @asset(group_name="templates")
-def post_guess_the_player_quiz( quiz_player_age_nationality:list, quiz_player_age_team: list, quiz_player_shirt_number: list) -> bool:
+def post_guess_the_player_quiz(quiz_player_height:list, quiz_player_2_clubs_played: list, quiz_player_age_nationality:list, quiz_player_age_team: list, quiz_player_shirt_number: list) -> bool:
     title = "Guess the player"
     description = "Guess 10 football players from the Premier League"
-    combined_q_list = quiz_player_age_nationality + quiz_player_age_team + quiz_player_shirt_number
+    combined_q_list = quiz_player_height + quiz_player_2_clubs_played+ quiz_player_age_nationality + quiz_player_age_team + quiz_player_shirt_number
     random.shuffle(combined_q_list)
     result_list = combined_q_list[:10]
     quiz_type=0
