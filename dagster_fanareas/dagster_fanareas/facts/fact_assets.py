2b65b76594658c55a2e6fba0c68c05ae7fcb2fba
ilyapetruhnov <petrukha16@gmail.com>
2024-03-12 12:52:53 +0100

    facts_by_team

--- dagster_fanareas/dagster_fanareas/facts/fact_assets.py
+++ dagster_fanareas/dagster_fanareas/facts/fact_assets.py
@@ -4,9 +4,11 @@ import random
 from dagster_fanareas.facts.queries import top_players_query, top_teams_query
 
 query = top_players_query
+query_teams = top_teams_query
 season = random.randint(2008,2023)
 top_n = 10
 metric_list = ['goals','assists','yellow_cards','penalties']
+teams_metric_list = ['goals','assists','yellow_cards','penalties','minutes_played','red_cards']
 
 @asset(group_name="facts")
 def publish_all_facts():
@@ -20,4 +22,11 @@ def publish_one_fact():
     facts_obj = Facts(query, season, top_n, metric_list)
     metric = random.choice(metric_list)
     facts_obj.post_facts(metric)
+    return True
+
+@asset(group_name="facts")
+def publish_one_fact_by_team():
+    facts_obj = Facts(query_teams, season, top_n, teams_metric_list)
+    metric = random.choice(metric_list)
+    facts_obj.post_facts(metric, by_team=True)
     return True
