7fa27c99e8b3e0f6caca1e6f461079bf4e57c3de
ilyapetruhnov <petrukha16@gmail.com>
2024-04-26 22:49:47 +0200

    demo_quiz_fix

--- dagster_fanareas/dagster_fanareas/quizzes/quizzes.py
+++ dagster_fanareas/dagster_fanareas/quizzes/quizzes.py
@@ -375,7 +375,7 @@ class Quizzes:
     def generate_player_shirt_number_question(self):
         df = self.generate_df(query_player_shirt_number)
         generated_team = self.get_team_name_and_id()
-        teamid = generated_team['team_id']
+        teamid = str(generated_team['team_id'])
         team = generated_team['team_name']
         sample_df = df[df['team_id'] == teamid]
         sample_df = sample_df.sample(4)
