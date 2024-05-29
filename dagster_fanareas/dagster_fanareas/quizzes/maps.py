8f07414d7f03d7b1e2bfcce4893e18f37ea2d679
ilyapetruhnov <petrukha16@gmail.com>
2024-05-29 15:21:42 +0200

    bugfix

--- dagster_fanareas/dagster_fanareas/quizzes/maps.py
+++ dagster_fanareas/dagster_fanareas/quizzes/maps.py
@@ -2,5 +2,5 @@ top_n_metric_map = {'goals': 10,
                     'yellow_cards': 10,
                     'appearances': 30,
                     'assists': 10,
-                    'goal_assists': 15,
+                    'goals_assists': 15,
                     'substitute_appearances': 10}
